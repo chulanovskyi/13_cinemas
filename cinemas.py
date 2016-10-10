a8b46264e55bcf02a01898b0c3df93b0b5c05d90
chulanovskyi <skyrocker@mail.ru>
2016-10-10 16:15:07 +0300

    half way on getting rating

--- cinemas.py
+++ cinemas.py
@@ -2,12 +2,13 @@ import requests
 import re
 from bs4 import BeautifulSoup as BS
 ''' [
-        {'movie_name':
-             {'rating': INT,
-             'cinemas_count': INT'
+        {'movie_title':
+             {'in_cinemas': INT',
+              'rating': INT
              }
         },
-    ...]
+        ...
+    ]
 '''
 def fetch_afisha_page():
     page = requests.get('http://www.afisha.ru/msk/schedule_cinema/').text
@@ -19,13 +20,25 @@ def parse_afisha_list(raw_html):
     movies_list = bs.find(id='schedule').find_all(class_='m-disp-table')
     movies_info = dict()
     for movie in movies_list:
+        movie_title = movie.h3.string
         cinemas = movie.next_sibling.next_sibling.find_all(class_='b-td-item')
-        movies_info[movie.h3.string] = {'cinemas' : len(cinemas)}
+        movies_info[movie_title] = {'in_cinemas' : len(cinemas)}
+    return movies_info
 
     
 def fetch_movie_info(movie_title):
-    pass
-
+    query_params = {'kp_query': movie_title}
+    search_query = requests.get('https://www.kinopoisk.ru/index.php',
+                                params=query_params).text
+    bs = BS(search_query, 'html.parser')
+    movie_info = bs.find(class_='element most_wanted')
+    if movie_info:            
+        rating = movie_info.find(class_='rating')
+    if rating:
+        votes = rating['title'].replace(u'\xa0',u'')
+    print(type(rating['title']))
+    print(votes)
+        
 
 def output_movies_to_console(movies):
     pass
@@ -34,3 +47,6 @@ def output_movies_to_console(movies):
 if __name__ == '__main__':
     afisha_page = fetch_afisha_page()
     parsed_afisha = parse_afisha_list(afisha_page)
+    test_movie = list(parsed_afisha.keys())[0] 
+    print(test_movie)
+    fetch_movie_info(test_movie)
