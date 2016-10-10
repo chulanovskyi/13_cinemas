538e9e54541bc4f024466899571f42126e618afa
chulanovskyi <skyrocker@mail.ru>
2016-10-10 14:14:10 +0300

    parse_afisha ready

--- cinemas.py
+++ cinemas.py
@@ -1,13 +1,28 @@
-
-
+import requests
+import re
+from bs4 import BeautifulSoup as BS
+''' [
+        {'movie_name':
+             {'rating': INT,
+             'cinemas_count': INT'
+             }
+        },
+    ...]
+'''
 def fetch_afisha_page():
-    pass
-
+    page = requests.get('http://www.afisha.ru/msk/schedule_cinema/').text
+    return page
+    
 
 def parse_afisha_list(raw_html):
-    pass
+    bs = BS(raw_html, 'html.parser')
+    movies_list = bs.find(id='schedule').find_all(class_='m-disp-table')
+    movies_info = dict()
+    for movie in movies_list:
+        cinemas = movie.next_sibling.next_sibling.find_all(class_='b-td-item')
+        movies_info[movie.h3.string] = {'cinemas' : len(cinemas)}
 
-
+    
 def fetch_movie_info(movie_title):
     pass
 
@@ -17,4 +32,5 @@ def output_movies_to_console(movies):
 
 
 if __name__ == '__main__':
-    pass
+    afisha_page = fetch_afisha_page()
+    parsed_afisha = parse_afisha_list(afisha_page)
