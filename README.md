132c938661dbb4a409e6d0589dc3ba70bde0172d
chulanovskyi <skyrocker@mail.ru>
2016-10-20 14:36:54 +0300

    fix after remarks

--- README.md
+++ README.md
@@ -16,3 +16,29 @@ You can edit in-script constants to get result as you want.
 | Constant | Description |
 | --- | --- |
 | `MOVIES_TO_PRINT` | how many top movies need to be printed|
+
+
+Что не так
+> You can edit in-script constants to get result as you want.
+Попробуй вынести их в args, скрипт станет удобнее в использовании
+
+def parse_afisha_list(raw_html):
+...
+return movies_info
+название плохо доносит смысл происходящего. Слишком общо и неконкретно
+
+Вынеси в urls в глобальные константы
+
+fetch_movie_info меняет входящий аргумент info. Такого быть не должно
+https://devman.org/encyclopedia/decomposition/decomposition_pure_functions/
+
+В python3 не нужна конструкция u'некий текст не на латинице'
+
+get_sort_attr - плохое название, используй лучше input_sort_attrs - оно говорит о том что функция взаимодействует с вводом/выводом
+
+get_sort_attr
+...
+exit(11)
+это было неожиданно... а раз так - перепиши, код должен быть легко предсказуем, что снижает риск ошибки при его сопровождении/доработке
+
+Магические числа – зло. Вынеси непонятную константу в переменную с говорящим названием. ('\xa0').
--- cinemas.py
+++ cinemas.py
@@ -1,19 +1,53 @@
 import requests
 import re
+from argparse import ArgumentParser
 from bs4 import BeautifulSoup as BS
-from operator import itemgetter
 
 
-MOVIES_TO_PRINT = 10
+AFISHA_URL = 'http://www.afisha.ru/msk/schedule_cinema/'
+KINOPOISK_URL = 'https://www.kinopoisk.ru/index.php'
+NO_BREAK_SPACE = '\xa0'
 
 
-def replace_all(text, cut_items, new_item):
-    for item in cut_items:
-        text = text.replace(item, new_item)
-    return text
+def fetch_afisha_page():
+    page = requests.get(AFISHA_URL).text
+    return page
+    
+
+def parse_afisha_list(raw_html):
+    bs = BS(raw_html, 'html.parser')
+    movies_list = bs.find(id='schedule').find_all(class_='m-disp-table')
+    movies_titles_and_meta = dict()
+    for movie in movies_list:
+        movie_title = movie.h3.string
+        cinemas = movie.next_sibling.next_sibling.find_all(class_='b-td-item')
+        movies_titles_and_meta[movie_title] = {'cinemas' : len(cinemas)}
+    return movies_titles_and_meta
 
 
-def get_sort_attr():
+def fetch_movie_info(title, info):
+    movie = {title: info}
+    movie[title].update({'rating': 0, 'votes': 0})
+    query_params = {'kp_query': title}
+    search_query = requests.get(KINOPOISK_URL, params=query_params).text
+    bs = BS(search_query, 'html.parser')
+    kp_info = bs.find(class_='element most_wanted')
+    if not kp_info:
+        return movie
+    rating_block = kp_info.find(class_='rating')
+    if not rating_block:
+        return movie
+    cut_items = [NO_BREAK_SPACE,'(',')']
+    try:
+        rating, votes = replace_all(rating_block['title'], cut_items, '').split()
+    except ValueError:
+        rating = rating_block.string
+        votes = replace_all(rating_block['title'], cut_items, '')
+    movie[title].update({'rating': float(rating), 'votes': int(votes)})
+    return movie
+
+
+def input_sort_attr():
     SORT_RATING, SORT_CINEMAS = (1,2)
     sort_by = input('Sort movies by:\n' +
                     '{by_rating}. Rating\n{by_cinemas}. Cinemas\n'.format(
@@ -24,46 +58,8 @@ def get_sort_attr():
     elif sort_by == '2':
         return 'cinemas'
     else:
-        print('Allowed inputs: 1, 2')
-        exit(11)
-
-
-def fetch_afisha_page():
-    page = requests.get('http://www.afisha.ru/msk/schedule_cinema/').text
-    return page
-    
-
-def parse_afisha_list(raw_html):
-    bs = BS(raw_html, 'html.parser')
-    movies_list = bs.find(id='schedule').find_all(class_='m-disp-table')
-    movies_info = dict()
-    for movie in movies_list:
-        movie_title = movie.h3.string
-        cinemas = movie.next_sibling.next_sibling.find_all(class_='b-td-item')
-        movies_info[movie_title] = {'cinemas' : len(cinemas)}
-    return movies_info
-
-    
-def fetch_movie_info(title, info):
-    movie = {title: info}
-    query_params = {'kp_query': title}
-    search_query = requests.get('https://www.kinopoisk.ru/index.php',
-                                params=query_params).text
-    bs = BS(search_query, 'html.parser')
-    kp_info = bs.find(class_='element most_wanted')
-    if kp_info:
-        rating_block = kp_info.find(class_='rating')
-    if not rating_block:
-        info.update({'rating': 0, 'votes': 0})
-        return movie
-    cut_items = [u'\xa0','(',')']
-    try:
-        rating, votes = replace_all(rating_block['title'], cut_items, '').split()
-    except ValueError:
-        rating = rating_block.string
-        votes = replace_all(rating_block['title'], cut_items, '')
-    info.update({'rating': float(rating), 'votes': int(votes)})
-    return movie
+        raise ValueError('Wrong input: {choice}\nAllowed inputs: 1, 2'.format(
+            choice=sort_by))
 
 
 def sort_movies(movies, attr):
@@ -81,12 +77,34 @@ def output_movies_to_console(movies, attr):
             attr=info[attr]))
 
 
+def replace_all(text, cut_items, new_item):
+    for item in cut_items:
+        text = text.replace(item, new_item)
+    return text
+
+
+def create_parser():
+    parser = ArgumentParser()
+    parser.add_argument(
+        '--movies',
+        type=int,
+        default=10,
+        help='Output movies list size')
+    return parser
+
+
 if __name__ == '__main__':
+    parser = create_parser()
+    output_list_size = parser.parse_args().movies
     afisha_page = fetch_afisha_page()
-    sort_attr = get_sort_attr()
+    try:
+        sort_attr = input_sort_attr()
+    except ValueError as err:
+        print(err)
+        exit(11)
     print('Parsing and sorting...')
     parsed_afisha = parse_afisha_list(afisha_page)
     for title, info in parsed_afisha.items():
         parsed_afisha.update(fetch_movie_info(title, info))
-    sorted_afisha = sort_movies(parsed_afisha, sort_attr)[:MOVIES_TO_PRINT]
+    sorted_afisha = sort_movies(parsed_afisha, sort_attr)[:output_list_size]
     output_movies_to_console(sorted_afisha, sort_attr)
